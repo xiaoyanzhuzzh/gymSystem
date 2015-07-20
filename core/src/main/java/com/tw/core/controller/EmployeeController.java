caa50088780f206abff93903810c59e19ee0aef1
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-20 17:15:33 +0800

    delete update employee controller

--- core/src/main/java/com/tw/core/controller/EmployeeController.java
+++ core/src/main/java/com/tw/core/controller/EmployeeController.java
@@ -27,44 +27,6 @@ public class EmployeeController {
         }
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.GET)
-    public ModelAndView getCreateUserPage(HttpServletRequest request){
-
-        if(request.getSession().getAttribute("currentUser") == null){
-
-            return new ModelAndView("redirect:/login");
-        } else {
-
-            return new ModelAndView("createUser");
-        }
-    }
-
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    public ModelAndView createUser(@RequestParam String name,
-                                   @RequestParam String gender,
-                                   @RequestParam int age,
-                                   @RequestParam String email,
-                                   @RequestParam String password){
-
-//        User user = new User(name, gender, age, email, EncryptionHelper.md5(password));
-//        userService.createUser(user);
-        return new ModelAndView("redirect:/users/");
-    }
-
-    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
-    public ModelAndView deleteUser(@PathVariable int id,
-                                   HttpServletRequest request){
-
-        if(request.getSession().getAttribute("currentUser") == null){
-
-            return new ModelAndView("redirect:/login");
-        } else {
-
-            employeeService.deleteEmployeeById(id);
-            return new ModelAndView("redirect:/employees/");
-        }
-    }
-
     @RequestMapping(value = "/update/{id}", method = RequestMethod.GET)
     public ModelAndView getUpdateUserAge(@PathVariable int id,
                                          HttpServletRequest request){
@@ -75,21 +37,9 @@ public class EmployeeController {
         } else {
 
             Employee employee = employeeService.getEmployeeById(id);
+            System.out.println(employee);
             return new ModelAndView("updateEmployee", "employee", employee);
         }
         //根据employee中的user_id去User表中去找到user,将其放到页面
-    }
-
-    @RequestMapping(value = "/update", method = RequestMethod.POST)
-    public ModelAndView UpdateUser(@RequestParam int id,
-                                   @RequestParam String name,
-                                   @RequestParam String gender,
-                                   @RequestParam int age,
-                                   @RequestParam String email,
-                                   @RequestParam String password){
-//        User user = new User(id, name, gender, age, email, EncryptionHelper.md5(password));
-//        userService.updateUser(user);
-//
-        return new ModelAndView("redirect:/users/");
     }
 }
