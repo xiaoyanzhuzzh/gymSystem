5926e96f1855e7acccb4cbc34fead438872fe999
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-20 17:11:16 +0800

    can show create course page

--- core/src/main/java/com/tw/core/controller/CourseController.java
+++ core/src/main/java/com/tw/core/controller/CourseController.java
@@ -1,6 +1,8 @@
 package com.tw.core.controller;
 
+import com.tw.core.entity.Employee;
 import com.tw.core.service.CourseService;
+import com.tw.core.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,6 +17,8 @@ public class CourseController {
 
     @Autowired
     private CourseService courseService;
+    @Autowired
+    private EmployeeService employeeService;
 
     @RequestMapping(value="/", method= RequestMethod.GET)
     public ModelAndView getCourses(HttpServletRequest request) {
@@ -25,6 +29,18 @@ public class CourseController {
         } else {
 
             return new ModelAndView("courses", "courses", courseService.getCourses());
+        }
+    }
+
+    @RequestMapping(value="/create", method= RequestMethod.GET)
+    public ModelAndView createCourse(HttpServletRequest request) {
+
+        if(request.getSession().getAttribute("currentUser") == null){
+
+            return new ModelAndView("redirect:/login");
+        } else {
+
+            return new ModelAndView("createCourse", "coaches", employeeService.getAllCoaches());
         }
     }
 }
