a2b92a7b69af254a366c32c59d5aede71f45a147
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-20 14:07:42 +0800

    can getCourses page

--- core/src/main/java/com/tw/core/controller/CourseController.java
+++ core/src/main/java/com/tw/core/controller/CourseController.java
@@ -1,7 +1,30 @@
 package com.tw.core.controller;
 
-/**
- * Created by zhihuizhang on 7/20/15.
- */
+import com.tw.core.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(value="/courses")
 public class CourseController {
+
+    @Autowired
+    private CourseService courseService;
+
+    @RequestMapping(value="/", method= RequestMethod.GET)
+    public ModelAndView getCourses(HttpServletRequest request) {
+
+        if(request.getSession().getAttribute("currentUser") == null){
+
+            return new ModelAndView("redirect:/login");
+        } else {
+
+            return new ModelAndView("courses", "courses", courseService.getCourses());
+        }
+    }
 }
