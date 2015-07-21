ab866bbee4845c679dac78b77daeff1bab9063a6
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-21 09:16:00 +0800

    add getSchedules method

--- core/src/main/java/com/tw/core/controller/ScheduleController.java
+++ core/src/main/java/com/tw/core/controller/ScheduleController.java
@@ -1,7 +1,30 @@
 package com.tw.core.controller;
 
-/**
- * Created by zhihuizhang on 7/21/15.
- */
+import com.tw.core.service.ScheduleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(value = "/schedules")
 public class ScheduleController {
+
+    @Autowired
+    private ScheduleService scheduleService;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getSchedules(HttpServletRequest request) {
+
+        if(request.getSession().getAttribute("currentUser") == null){
+
+            return new ModelAndView("redirect:/login");
+        } else {
+
+            return new ModelAndView("schedules", "schedules", scheduleService.getSchedules());
+        }
+    }
 }
