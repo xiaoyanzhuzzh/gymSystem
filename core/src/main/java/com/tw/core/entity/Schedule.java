3830c6d510480ccdddc8527b7c1760246182aa0e
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-21 08:58:30 +0800

    add Schedule codes

--- core/src/main/java/com/tw/core/entity/Schedule.java
+++ core/src/main/java/com/tw/core/entity/Schedule.java
@@ -1,7 +1,58 @@
 package com.tw.core.entity;
 
-/**
- * Created by zhihuizhang on 7/20/15.
- */
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "schedules")
 public class Schedule {
+
+    private int id;
+    private Date time;
+    private Course course;
+
+    public Schedule() {
+    }
+
+    public Schedule(Date time, Course course) {
+
+        this.time = time;
+        this.course = course;
+    }
+
+    public Schedule(int id, Date time, Course course) {
+        this.id = id;
+        this.time = time;
+        this.course = course;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "time")
+    public Date getTime() {
+        return time;
+    }
+
+    @ManyToOne(cascade=CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
 }
