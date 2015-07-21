8c58a978eba3ded098c2b3062a635abe0d6fd95b
xiaoyanzhuzzh <xiaoyanzhuzzh@sina.com>
2015-07-21 18:45:53 +0800

    create Customer and Relation entity

---
+++ core/src/main/java/com/tw/core/dao/CustomerDao.java
@@ -0,0 +1,7 @@
+package com.tw.core.dao;
+
+/**
+ * Created by zhihuizhang on 7/21/15.
+ */
+public class CustomerDao {
+}
---
+++ core/src/main/java/com/tw/core/entity/CourseCustomerRelation.java
@@ -0,0 +1,7 @@
+package com.tw.core.entity;
+
+/**
+ * Created by zhihuizhang on 7/21/15.
+ */
+public class CourseCustomerRelation {
+}
